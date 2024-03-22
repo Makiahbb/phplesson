46566d72ea6c9ac12869642b5b69a6ccd722e8a8
Makiah Burns <110932133+Makiahbb@users.noreply.github.com>
2024-03-22 07:49:43 -0500

    vrrgeog

---
+++ datatypes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stuart</title>
+</head>
+<body>
+<?php
+       $name = 'Coding the code like a coder';
+       //Integer
+       $name = 15;
+       //Float
+       $name = 20.7366;
+       //Boolean
+       
+       //Array
+       $names = array("Kevin","Bob","Stuart");
+       echo $names['1']
+    ?>
+</body>
+</html>
