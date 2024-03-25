3b2a76b45942f96642af8c75ce3e42dbf4474cf9
Makiah Burns <110932133+Makiahbb@users.noreply.github.com>
2024-03-25 07:56:15 -0500

    jyddrymdyrk

---
+++ operators.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operators</title>
+</head>
+<body>
+    <?php
+    //Vid1
+    echo 5**8,5/2,9*3;
+    ?>
+    <?php
+    //Vid2
+    $x = 100;
+    $x /= 20
+    echo $x;
+    ?>
+    <?php
+    //Vid3
+    $y = "10";
+    $x = 10;
+
+    if ($x <> $y)   {
+        echo "True!";
+    }
+    
+    else  {
+        echo "False!";
+    }
+    ?>
+    <?php
+    //Vid4
+    $x = 10;
+    echo $x--;
+    echo $x;
+    ?>
+    <?php
+    //Vid5
+    $y = 20;
+    $x = 20;
+
+    if ($x === $y or 1 == 1)   {
+        echo "True";
+    }
+    ?>
+</body>
+</html>
