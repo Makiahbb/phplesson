242b71aff92c5228cdc71109d78cbef27fd7b554
Makiah Burns <110932133+Makiahbb@users.noreply.github.com>
2024-03-27 08:27:46 -0500

    ttjyrdj

---
+++ conditionals.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x = "1";
+
+    if ($x == "1") {
+        echo "The dog is blue";
+    }
+    elseif ($x == "2") {
+        echo "The dog is a little blue";
+    }
+    elseif ($x == "3") {
+        echo "The dog is a little blue";
+    }
+    elseif ($x == "4") {
+        echo "The dog is a little blue";
+    }
+    else {
+        echo "The dog is red";
+    }
+    ?>
+</body>
+</html>
