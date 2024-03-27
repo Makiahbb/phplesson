9846e16b2d2f41f0079d27241a08f309411d080f
Makiah Burns <110932133+Makiahbb@users.noreply.github.com>
2024-03-27 08:39:56 -0500

    tytmmt

--- conditionals.php
+++ conditionals.php
@@ -25,5 +25,26 @@
         echo "The dog is red";
     }
     ?>
+ <br>
+    <?php
+    $x = 8;
+
+    switch ($x) {
+        case 1:
+            echo "The answer is 1";
+    break;
+        case 2:
+            echo "The answer is 2";
+    break;
+        case 3:
+            echo "The answer is 3";
+    break;
+        case 4:
+        echo "The answer is 4";
+    break;
+    default:
+        echo "There is no answer";
+    }
+    ?>
 </body>
 </html>
